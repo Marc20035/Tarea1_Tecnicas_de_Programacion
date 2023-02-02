c5ff9146b85f67286f2eeb323f3a1c8adefecaa2
tHeCreator <marcsabalete@gmail.com>
2023-02-02 20:54:59 +0000

    comit3

--- Pregunta2.java
+++ Pregunta2.java
@@ -1,3 +1,23 @@
 //¿Qué algoritmos crees que usan por tus juegos y aplicaciones favoritas?
 
 /*Uno de los algoritmos principales en juegos,aplicaciones es por ejemplo la verificacion de usuario a la hora de ingresar a los mismos*/ 
+//Un ejemplo es el siguiente:
+class Pregunta2{
+    public static void main(String[] args){
+        String usuario = "admin";
+        String clave = "1234";
+        if(usuario.equals("admin") && clave.equals("1234")){
+            System.out.println("Bienvenido al sistema");
+        }else{
+            System.out.println("Usuario o clave incorrecta");
+        }
+    }
+}
+
+
+
+
+
+
+
+
